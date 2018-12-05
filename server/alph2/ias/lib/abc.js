97da396ec95091b9736cd7fc404ebbab43c5b1ca
mayank-kansal15 <mayank.kansal@talentica.com>
2018-12-05 11:20:51 +0530

    Update abc.js

--- server/alph2/ias/lib/abc.js
+++ server/alph2/ias/lib/abc.js
@@ -1,5 +1,5 @@
 /**
- * @api {get} /v1/config Get an IAS config value from ETCD.
+ * @api {get} /v1/config Get an IAS config value from   ETCD.
  * @apiName GetConfigValue
  * @apiGroup General
  * @apiParam (Query) {String} key Name of the ETCD config key stored under /ias
